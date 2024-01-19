715c5c4e04736376a09c37e8f002ceeada3f5613
Oswaldo Ravelo <raveloswaldo@gmail.com>
2024-01-19 19:37:37 -0300

    Sets up the project

---
+++ petadoption/src/test/java/org/api/studies/petadoption/PetadoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.studies.petadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetadoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
