8833da56c7184ae6b24f89a90c5dd6c0f5c28787
Oswaldo Ravelo <raveloswaldo@gmail.com>
2024-01-20 21:50:03 -0300

    Catalog of videos api

---
+++ videoscatalog/src/main/java/org/studies/videoscatalog/VideocatalogApplication.java
@@ -0,0 +1,13 @@
+package org.studies.videoscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocatalogApplication.class, args);
+	}
+
+}
---
+++ videoscatalog/src/test/java/org/studies/videoscatalog/VideocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.studies.videoscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
