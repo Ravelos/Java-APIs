ba0e328007d8918ffa9cb5231346e371d3f75f0b
Oswaldo Ravelo <raveloswaldo@gmail.com>
2024-01-22 18:16:45 -0300

    Creates entity class

--- petadoption/src/main/java/org/api/studies/petadoption/domain/entity/PetRegistration.java
+++ petadoption/src/main/java/org/api/studies/petadoption/domain/entity/PetRegistration.java
@@ -1,2 +1,14 @@
-package org.api.studies.petadoption.domain.entity;public class PetRegistration {
+package org.api.studies.petadoption.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetRegistration {
+
+
 }
