c7fb7ddf288fb3d165dccc557e6c38d7cef68de8
Oswaldo Ravelo <raveloswaldo@gmail.com>
2024-01-25 14:53:10 -0300

    Creates invalida adoption exception

--- petadoption/src/main/java/org/api/studies/petadoption/exception/InvalidAdoptionException.java
+++ petadoption/src/main/java/org/api/studies/petadoption/exception/InvalidAdoptionException.java
@@ -1,2 +1,11 @@
-package org.api.studies.petadoption.exception;public class InvalidAdoptionException {
+package org.api.studies.petadoption.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.I_AM_A_TEAPOT)
+public class InvalidAdoptionException extends RuntimeException{
+    public InvalidAdoptionException(String message){
+        super(message);
+    }
 }
