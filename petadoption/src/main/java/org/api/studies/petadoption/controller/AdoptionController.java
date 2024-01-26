9f277e5652922bcfc513a0c9afa204fb3d6f73b6
oswaldo_ravelo <oswaldo@opportunityatwork.org>
2024-01-25 21:01:57 -0300

    Creates controller for pet adoption api

--- petadoption/src/main/java/org/api/studies/petadoption/controller/AdoptionController.java
+++ petadoption/src/main/java/org/api/studies/petadoption/controller/AdoptionController.java
@@ -1,8 +1,35 @@
 package org.api.studies.petadoption.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.api.studies.petadoption.domain.dto.CreateAdoptionRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AdoptionController {
+    @GetMapping(value = "api/petadoptions")
+    public List<String> Listar() {
+        List<String> petList = new ArrayList<String>();
+        petList.add("Firu");
+        petList.add("Nerón");
+        return petList;
+    }
+
+    @GetMapping(value = "api/petadoptions/{id}")
+    public String searchById(@PathVariable Integer id){
+        return "Princesa " + id;
+    }
+
+    @GetMapping(value = "api/petadoptions/{name}")
+    public String searchByName(@PathVariable String name){
+        return "Chatram " + name;
+    }
+
+    @PostMapping(value = "api/petadoptions")
+    public void createPetRegistration(@RequestBody @Valid CreateAdoptionRequest petRegistration) {
+        return;
+    }
 
 }
